1942402369dd96a6394de63724cbc7deb660e385
AlekseyMG <vinni-pux@yandex.ru>
2024-12-22 00:32:49 +0300

    3 day

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -6,8 +6,12 @@ import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.util.Date;
+
 @Data
 public class Order {
+    private Long id;
+    private Date placedAt;
     @NotBlank(message="Name is required")
     private String name;
     @NotBlank(message="Street is required")
@@ -24,4 +28,7 @@ public class Order {
     private String ccExpiration;
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
+    public void addDesign(Taco saved) {
+    }
 }
