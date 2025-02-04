33bce0bbe87e04d75a21fb57afc73108cbfd7e42
AlekseyMG <vinni-pux@yandex.ru>
2025-02-04 18:25:50 +0300

    5 day

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -1,38 +1,63 @@
 package tacos;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-public class Order {
+@Entity
+@Table(name = "Taco_Order")
+public class Order implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date placedAt;
+
     @NotBlank(message="Name is required")
     private String name;
+
     @NotBlank(message="Street is required")
     private String street;
+
     @NotBlank(message="City is required")
     private String city;
+
     @NotBlank(message="State is required")
     private String state;
+
     @NotBlank(message="Zip code is required")
     private String zip;
+
     @CreditCardNumber(message="Not a valid credit card number")
     private String ccNumber;
+
     @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message="Must be formatted MM/YY")
     private String ccExpiration;
+
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+
+    @ManyToMany(targetEntity = Taco.class)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addDesign(Taco taco) {
         this.tacos.add(taco);
+    }
+
+    @PrePersist
+    void placedAt() {
+        this.placedAt = new Date();
     }
 }
