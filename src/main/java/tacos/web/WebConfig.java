28fac7622140ed3a11fd58223b0d65196c90110f
AlekseyMG <vinni-pux@yandex.ru>
2025-03-24 01:09:34 +0300

    7 day

--- src/main/java/tacos/web/WebConfig.java
+++ src/main/java/tacos/web/WebConfig.java
@@ -1,10 +1,13 @@
 package tacos.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.hateoas.config.EnableHypermediaSupport.*;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableHypermediaSupport(type = {HypermediaType.HAL})
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
